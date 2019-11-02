ab89929724ad76c00adc7225a3c1f66b4c37059a
Cyrille Rossant <rossant@users.noreply.github.com>
2019-11-02 15:13:30 +0100

    Simplify file organization

--- npytools.py
+++ npytools.py
@@ -9,8 +9,54 @@
 
 import click
 import numpy as np
+import atexit
+import logging
+import os
+import os.path as op
+from pathlib import Path
+import subprocess
+import sys
+
+from io import StringIO
 
 np.set_printoptions(precision=4, suppress=True, edgeitems=2, threshold=50)
+
+
+def _git_version():
+    """Return the git version."""
+    curdir = os.getcwd()
+    os.chdir(str(Path(__file__).parent))
+    try:
+        with open(os.devnull, 'w') as fnull:
+            version = ('-git-' + subprocess.check_output(
+                       ['git', 'describe', '--abbrev=8', '--dirty', '--always', '--tags'],
+                       stderr=fnull).strip().decode('ascii'))
+            return version
+    except (OSError, subprocess.CalledProcessError):  # pragma: no cover
+        return ""
+    finally:
+        os.chdir(curdir)
+
+
+__author__ = 'Cyrille Rossant'
+__email__ = 'cyrille.rossant at gmail.com'
+__version__ = '0.1.0'
+__version_git__ = __version__ + _git_version()
+
+
+# Set a null handler on the root logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.NullHandler())
+logger.propagate = False
+
+
+@atexit.register
+def on_exit():  # pragma: no cover
+    # Close the logging handlers.
+    for handler in logger.handlers:
+        handler.close()
+        logger.removeHandler(handler)
 
 
 #------------------------------------------------------------------------------
